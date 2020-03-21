380529e88fac18042671bfd068ec2a03387273ba
NerdPraise <praiseajayi2@gmail.com>
2020-03-21 14:16:23 +0100

    Js Task 1

---
+++ JS codes/JsTask1.js
@@ -0,0 +1,10 @@
+let name = "Ajayi Praise";
+const courses = ["Python", "HTML", "JS", "CSS"];
+
+arrlen = courses.length;
+//number of courses is 4, even
+for (i=1; i<201; i++){
+    if (i%2 == 0){
+        console.log(i) 
+    }
+}
