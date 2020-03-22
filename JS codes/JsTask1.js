eff42f1751a5cc818d4739222ee5d2b29104f689
NerdPraise <praiseajayi2@gmail.com>
2020-03-22 16:16:21 +0100

    actual file

--- JS codes/JsTask1.js
+++ JS codes/JsTask1.js
@@ -1,6 +1,11 @@
 let name = "Ajayi Praise";
 const courses = ["Python", "HTML", "JS", "CSS"];
 
+console.log(name);
+for (i of courses){
+    console.log(i)
+}
+
 arrlen = courses.length;
 //number of courses is 4, even
 for (i=1; i<201; i++){
