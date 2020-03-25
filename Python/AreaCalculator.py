5063d933d392d168d4e00b2ff5466fd8e90794b8
NerdPraise <praiseajayi2@gmail.com>
2020-03-25 13:08:35 +0100

    Added python project

---
+++ Python/AreaCalculator.py
@@ -0,0 +1,10 @@
+# A program to compute the area of a circle with a given radii
+import math
+
+def calculator():
+    radius = int(input("What is the radius of the circle "))
+    area = (math.pi * (radius**2))
+    area = round(area, 2)
+    print(area)
+
+calculator()
