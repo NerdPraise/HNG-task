034f63d9b1e50b32addade5ab5015a06cd378c19
NerdPraise <praiseajayi2@gmail.com>
2020-04-16 19:02:26 +0100

    new js file

---
+++ JS codes/JsTask3.js
@@ -0,0 +1,28 @@
+const numdiv = (number) => {
+    numArray = []
+    for(count=1; count<=number; count++){
+        value =  count;
+        stringValue = [];
+        if (count%2 == 0){
+            stringValue.push("yu");
+
+        }
+        if (count%3 == 0){
+            stringValue.push("gi");
+        }
+        if (count%5 == 0){
+            stringValue.push("oh");
+        }
+        if (stringValue.length != 0){
+            value = stringValue.join("-");
+        }
+        else {
+            value = count
+        }
+        numArray.push(value);
+        
+    }
+    console.log(numArray)
+}
+numdiv(100)
+numdiv(30)
